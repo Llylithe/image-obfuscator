bbe0980371dc80f708d735e5a0459ae74fde814e
Llylithe <81000091+Llylithe@users.noreply.github.com>
2025-05-23 14:53:08 +1000

    aaaaaaaaaaaaaa

--- main.py
+++ main.py
@@ -12,19 +12,19 @@ def generate_shares(image_path):
     share_b = (temp_data - temp_a) % 256
     share_b = share_b.astype(np.uint8)
 
-    Image.fromarray(share_a).save("share_a.png")
-    Image.fromarray(share_b).save("share_b.png")
+    Image.fromarray(share_a).save("obfu_1.png")
+    Image.fromarray(share_b).save("obfu_2.png")
 
-    print("Shares generated: share_a.png, share_b.png")
+    print("Shares generated: obfu_1.png, obfu_2.png")
 
 def reconstruct_image(share_a_path, share_b_path):
     a = np.array(Image.open(share_a_path).convert("RGB"), dtype=np.uint16)
     b = np.array(Image.open(share_b_path).convert("RGB"), dtype=np.uint16)
 
     reconstructed = (a + b) % 256
-    Image.fromarray(reconstructed.astype(np.uint8)).save("reconstructed.png")
-    print("Reconstructed image saved as reconstructed.png")
+    Image.fromarray(reconstructed.astype(np.uint8)).save("not_obfu_anymore.png")
+    print("Reconstructed image saved as not_obfu_anymore.png")
 
-# Call the functions with string filenames
+# change bathtub.jpg to wtv
 generate_shares("bathtub.jpg")
-reconstruct_image("share_a.png", "share_b.png")
+reconstruct_image("obfu_1.png", "obfu_2.png")
